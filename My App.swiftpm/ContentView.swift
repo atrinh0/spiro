015ff4190fd28dddd4e5549bc8d71cd5f38301a0
An Trinh <atrinh@gmail.com>
2021-12-26 18:19:20 +0000

    Initial commit

---
+++ My App.swiftpm/ContentView.swift
@@ -0,0 +1,61 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
+    @State private var lineWidth = 1.0
+    @State private var animate = false
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            
+            ZStack {
+                Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                    .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: lineWidth)
+                    .blur(radius: 3)
+                
+                Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                    .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: lineWidth)
+            }
+            .rotationEffect(.degrees(animate ? 0 : 360))
+            .animation(.linear(duration: 60).repeatForever(autoreverses: false), value: animate)
+            .frame(width: 300, height: 300)
+            .onAppear {
+                animate = true
+            }
+            
+            Spacer()
+            
+            Group {
+                Group {
+                    Text("Inner radius: \(Int(innerRadius))")
+                    Slider(value: $innerRadius, in: 10...150, step: 1)
+                        .padding([.horizontal, .bottom])
+                
+                    Text("Outer radius: \(Int(outerRadius))")
+                    Slider(value: $outerRadius, in: 10...150, step: 1)
+                        .padding([.horizontal, .bottom])
+                    
+                    Text("Distance: \(Int(distance))")
+                    Slider(value: $distance, in: 1...150, step: 1)
+                        .padding([.horizontal, .bottom])
+                }
+                Text("Line width: \(lineWidth, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $lineWidth, in: 0.1...10, step: 0.1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
---
+++ My App.swiftpm/Spirograph.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        
+        return path
+    }
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    var a = a
+    var b = b
+    
+    while b != 0 {
+        let temp = b
+        b = a % b
+        a = temp
+    }
+    
+    return a
+}
